8367e95781c44f67b2b73253e0587ca61f93d2ae
­Jason Prime <600613.m6@gmail.com>
2024-04-01 09:24:54 +0200

    Header comment added with details
    Codename, Version, Description, Repo URL, Dev + contact details

--- TitanShield.js
+++ TitanShield.js
@@ -1,5 +1,17 @@
-// Check if the current window is the topmost window
-if (window !== window.top) {
-    // If not, redirect the top window to the current location
-    window.top.location.href = window.location.href;
-}
+/**
+ * TitanShield.js
+ * Version: 1.1.0 Beta
+ * Description: This script provides frame blocking functionality to prevent clickjacking attacks by ensuring the website is not loaded within an iframe.
+ * 
+ * Repository: https://github.com/Brainhub24/TitanShield
+ * Developer : Jan Gebser (Brainhub24)
+ * Contact   : github@brainhub24.com
+ */
+
+(function() {
+    // Check if the current window is the topmost window
+    if (window !== window.top) {
+        // If not, replace the URL of the top window with the current URL
+        window.top.location.href = window.location.href;
+    }
+})();
